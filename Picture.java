5df51bc0528042fd841422062d75f7da7ef90aaa
letvis <letvisales@ukr.net>
2017-11-08 21:01:34 +0200

    Рисунок2

---
+++ Picture.java
@@ -0,0 +1,22 @@
+public class Picture {
+    public static void main(String args[]) {
+
+
+        System.out.println("                                    █████████");
+        System.out.println("    ███████                 ███▒▒▒▒▒▒▒▒███");
+        System.out.println("   █▒▒▒▒▒▒█           ███▒▒▒▒▒▒▒▒▒▒▒▒▒███");
+        System.out.println("    █▒▒▒▒▒▒█       ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println("      █▒▒▒▒▒█     ██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███");
+        System.out.println("        █▒▒▒█     █▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██");
+        System.out.println("    █████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println("    █▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println(" ██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██");
+        System.out.println("██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒▒██");
+        System.out.println("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██");
+        System.out.println("██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println(" █▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println("  ██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+        System.out.println("    ████████████       █████████████████");
+    }
+}
+
