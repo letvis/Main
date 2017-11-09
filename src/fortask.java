b76c664daf09b766f37449671379f9660a1cdf11
IlliaKozhushko <illyakozshushko@gmail.com>
2017-11-09 20:33:58 +0200

    fortask

---
+++ src/fortask.java
@@ -0,0 +1,10 @@
+public class fortask {
+    public static void main(String[] args) {
+        System.out.print(">");
+        int x=5;
+        for(int i=0;i<x;i++){
+            System.out.print("-");
+        }
+        System.out.print(">");
+    }
+}
